9183cef8ca2ff70bd77d89d9a378d530ce276bff
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 21:09:23 -0400

    Update app.java

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -1,18 +1,31 @@
+
+
+// Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+
+//What is the input string? Homer
+//Homer has 5 characters
+
+//Be sure the output contains the original string.
+//Use a single output statement to construct the output.
+//Use a built-in function of the programming language to determine the length of the string.
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Korinne Ramcharitar
+ */
 package base;
 
+package b
 import java.util.Scanner;
 
-/*Create a program that prompts for your name and prints a greeting using your name.
+public class App {
+public static void main(String[] args) {
 
- */
-public class app {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = in.nextLine();
+    Scanner input = new Scanner(System.in);
+    System.out.println("What is the input");
+    String name = input.nextLine();
+    System.out.println(name + " has " + name.length() + " characters.");
+}
 
-        String outputString = "Hello, " + name + ", nice to meet you";
-        System.out.println(outputString);
 
-    }
 }
